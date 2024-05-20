039b9dbc71cbde30a623a795b300ce669b76b7e1
Mawuawoe <desmonddzakago@gmail.com>
2024-05-20 11:06:37 +0000

    test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -41,5 +41,18 @@ class test_BaseModel(unittest.TestCase):
     def test_2_models_diff_in_updated_at(self):
         self.assertLess(self.my_model.updated_at, self.my_model_2.updated_at)
 
+    def test_str_rep(self):
+        dt = datetime.datetime.today()
+        dt_repr = repr(dt)
+        bm = BaseModel()
+        bm.id = "12345"
+        bm.created_at = bm.updated_at = dt
+        bmstr = bm.__str__()
+        self.assertIn("[BaseModel] (12345)", bmstr)
+        self.assertIn("'created_at': " + dt_repr, bmstr)
+        self.assertIn("'updated_at': " + dt_repr, bmstr)
+        self.assertIn("'id': ", bmstr)
+
+
 if __name__ == '__main__':
     unittest.main()
