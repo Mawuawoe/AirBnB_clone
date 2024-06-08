7ab7fef1b246a67aeadc9c1ad250705c9d2c74e0
Mawuawoe <desmonddzakago@gmail.com>
2024-06-08 16:58:57 +0000

    task_test

--- console.py
+++ console.py
@@ -22,7 +22,7 @@ class HBNBCommand(cmd.Cmd):
                   "User",
                   "State",
                   "City",
-                  "Palce",
+                  "Place",
                   "Amenity",
                   "Review"]
 
@@ -61,7 +61,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
         else:
             new_instance = eval(f"{command[0]}()")
-            storage.save()
+            new_instance.save()
             print(new_instance.id)
 
     def do_show(self, arg):
@@ -99,6 +99,70 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
         elif len(command) < 2:
             print("** instance id missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(command[0], command[1])
+            if key in objects:
+                del objects[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances
+        based or not on the class name.
+        Ex: $ all BaseModel or $ all.
+        """
+        objects = storage.all()
+        command = split(arg)
+
+        if len(command) == 0:
+            for key, value in objects.items():
+                print(str(value))
+        elif command[0] not in self.ValidClass:
+            print("** class doesn't exist **")
+        else:
+            for key, value in objects.items():
+                if key.split('.')[0] == command[0]:
+                    print(str(value))
+
+    def do_update(self, arg):
+        """
+         Updates an instance based on the class name and id
+         by adding or updating attribute
+         (save the change into the JSON file).
+         Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com".
+        """
+        command = split(arg)
+
+        if len(command) == 0:
+            print("** class name missing **")
+        elif command[0] not in self.ValidClass:
+            print("** class doesn't exist **")
+        elif len(command) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(command[0], command[1])
+            if key not in objects:
+                print("** no instance found **")
+            elif len(command) < 3:
+                print("** attribute name missing **")
+            elif len(command) < 4:
+                print("** value missing **")
+            else:
+                obj = objects[key]
+                attr_name = command[2]
+                attr_value = command[3]
+
+                try:
+                    attr_value = eval(attr_value)
+                except Exception:
+                    pass
+
+                setattr(obj, attr_name, attr_value)
+                obj.save()
 
 
 if __name__ == '__main__':
